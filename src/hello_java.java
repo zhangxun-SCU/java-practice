c20145cbc919e8f42cd4655f299e591592074451
cw <1878506329@qq.com>
2022-08-25 16:32:00 +0800

    初识java

---
+++ src/hello_java.java
@@ -0,0 +1,9 @@
+
+//public class hello_java 表示这是一个公共的类·
+public class hello_java {
+
+//    一个main方法，执行入口
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}

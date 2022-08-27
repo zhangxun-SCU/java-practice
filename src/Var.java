4428b6a35fd73cda3a16a858cd1145a2fc981c68
cw <1878506329@qq.com>
2022-08-27 11:15:59 +0800

    8.27.11.15

---
+++ src/Var.java
@@ -0,0 +1,14 @@
+public class Var {
+    public static void main(String[] args){
+        int a;
+        a = 100;
+        System.out.println(a);
+        int b = 800;
+        System.out.println(b);
+        int age = 30;
+        double score = 88.9;
+        char gender = '男';
+        String name = "name";
+        byte i = 1;
+    }
+}
---
+++ src/comment.java
@@ -0,0 +1,9 @@
+/**
+ * @author cw
+ * @version 0.0
+ */
+public class comment {
+    public static void main(String[] args){
+
+    }
+}
